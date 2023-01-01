b3f929c3bc38af4690442d8679306c7871c10cdf
kill_in_sun <noneck.0311@gmail.com>
2023-01-01 11:39:43 +0900

    wip

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/killinsun/go-portaudio-study
 
 go 1.19
 
-require github.com/gordonklaus/portaudio v0.0.0-20221027163845-7c3b689db3cc
+require (
+	github.com/go-audio/wav v1.1.0
+	github.com/gordonklaus/portaudio v0.0.0-20221027163845-7c3b689db3cc
+)
+
+require (
+	github.com/go-audio/audio v1.0.0 // indirect
+	github.com/go-audio/riff v1.0.0 // indirect
+	github.com/icza/bitio v1.0.0 // indirect
+	github.com/mewkiz/flac v1.0.7 // indirect
+	github.com/mewkiz/pkg v0.0.0-20190919212034-518ade7978e2 // indirect
+)
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/gordonklaus/portaudio"
 )
@@ -15,25 +16,31 @@ func main() {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, os.Kill)
 
-	outputFileName := "./portaudioRecordings"
-	f, err := os.Create(outputFileName)
-	chk(err)
-
-	defer func() {
-		chk(f.Close())
-	}()
-
 	portaudio.Initialize()
 	defer portaudio.Terminate()
 
-	input := make([]int32, 64)
+	input := make([]int16, 64)
 	stream, err := portaudio.OpenDefaultStream(1, 0, 44100, len(input), input)
+	startTime := stream.Time()
 	chk(err)
 	defer stream.Close()
 
 	chk(stream.Start())
+	var f *os.File
+
 loop:
 	for {
+		elapseTime := (stream.Time() - startTime).Round(time.Second)
+
+		if int(elapseTime.Seconds())%3 == 0 {
+			outputFileName := fmt.Sprintf("./record_%d", int(elapseTime.Seconds()))
+			f, err = os.Create(outputFileName)
+			chk(err)
+			defer func() {
+				chk(f.Close())
+			}()
+		}
+
 		chk(stream.Read())
 		chk(binary.Write(f, binary.BigEndian, input))
 
