6497b80530cbee2f9a632bbb3876848e906a15c9
kill_in_sun <noneck.0311@gmail.com>
2023-01-05 07:13:08 +0900

    fix a bug

--- recorder/wav_encoder.go
+++ recorder/wav_encoder.go
@@ -10,18 +10,17 @@ import (
 
 type WAVEncoder struct {
 	writer     *wav.Writer
-	OutFile    *os.File
 	numSamples uint32
 	buf        []int16
 }
 
 func NewWAVEncoder(file *os.File, buf []int16) *WAVEncoder {
 	en := &WAVEncoder{
-		OutFile:    file,
 		numSamples: uint32(len(buf)),
+		buf:        buf,
 	}
 
-	en.writer = wav.NewWriter(en.OutFile, en.numSamples, 1, 44100, 16)
+	en.writer = wav.NewWriter(file, en.numSamples, 1, 44100, 16)
 	return en
 }
 
